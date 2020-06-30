db33211f27292282d7aa545bfbcfd5e339796f57
thormic <michal.thor5@gmail.com>
2020-06-30 18:08:24 +0200

    Add scorecard divisions

--- comparison.R
+++ comparison.R
@@ -4,6 +4,10 @@ load(file = "models/rf_model.Rdata")
 load(file = "models/gbm_model.Rdata")
 load(file = "models/xgb_model.Rdata")
 load(file = "models/result_table.RData")
+load(file = "models/train_result_table.RData")
+load(file = "models/test_table.RData")
+load(file = "models/whole_table.RData")
+source("model_functions.R")
 
 models <- c("Logit", "Random_Forest", "Adaboost", "XGBoost")
 
@@ -33,21 +37,84 @@ ginis <- c(logit_gini, rf_gini, gbm_gini, xgb_gini)
 
 # ------------------------------------------------------------------------------ K-S Tests
 # Logit
-logit_ks_score <- ks.test(result_table[result_table$def=='no',c("logit_score")],result_table[result_table$def=='yes',c("logit_score")])$statistic
+logit_ks_score <- round(ks.test(result_table[result_table$def=='no',c("logit_score")],result_table[result_table$def=='yes',c("logit_score")])$statistic, 4)
 # RF
-rf_ks_score <- ks.test(result_table[result_table$def=='no',c("rf_score")],result_table[result_table$def=='yes',c("rf_score")])$statistic
+rf_ks_score <- round(ks.test(result_table[result_table$def=='no',c("rf_score")],result_table[result_table$def=='yes',c("rf_score")])$statistic, 4)
 # Adaboost
-gbm_ks_score <- ks.test(result_table[result_table$def=='no',c("gbm_score")],result_table[result_table$def=='yes',c("gbm_score")])$statistic
+gbm_ks_score <- round(ks.test(result_table[result_table$def=='no',c("gbm_score")],result_table[result_table$def=='yes',c("gbm_score")])$statistic, 4)
 # XGBoost
-xgb_ks_score <- ks.test(result_table[result_table$def=='no',c("xgb_score")],result_table[result_table$def=='yes',c("xgb_score")])$statistic
+xgb_ks_score <- round(ks.test(result_table[result_table$def=='no',c("xgb_score")],result_table[result_table$def=='yes',c("xgb_score")])$statistic, 4)
 
 k_s_scores <- c(logit_ks_score, rf_ks_score, gbm_ks_score, xgb_ks_score)
+
+# ------------------------------------------------------------------------------ PSI Test
+# Logit
+logit_psi <- round(cal_psi(data1=train_result_table, data2=result_table, bench="logit_score",target="logit_score",bin=20), 4)
+# RF
+rf_psi <- round(cal_psi(data1=train_result_table, data2=result_table, bench="rf_score",target="rf_score",bin=20), 4)
+# Adaboost
+gbm_psi <- round(cal_psi(data1=train_result_table, data2=result_table, bench="gbm_score",target="gbm_score",bin=20), 4)
+# XGBoost
+xgb_psi <- round(cal_psi(data1=train_result_table, data2=result_table, bench="xgb_score",target="xgb_score",bin=20), 4)
+
+psis <- c(logit_psi, rf_psi, gbm_psi, xgb_psi)
 
 # ------------------------------------------------------------------------------ Optimal Cut-off
 
 # EXCUSE ME WHAT THE DUCK
 
 
+# ------------------------------------------------------------------------------ # Validation set results
+
 comparison_table <- as.data.frame(models)
-comparison_table <- cbind(comparison_table, aucs, ginis, k_s_scores)
-colnames(comparison_table) <- c("model_name", "auc", "gini", "k_s_stat")
+comparison_table <- cbind(comparison_table, aucs, ginis, k_s_scores, psis)
+colnames(comparison_table) <- c("model_name", "auc", "gini", "k_s_stat", "psi")
+
+View(comparison_table)
+
+
+# ------------------------------------------------------------------------------ # Test set results GINI
+
+# Logit
+logit_gini <- round(2*auc(test_table$def, test_table$logit_model, direction="<")-1, 4)
+# RF
+rf_gini <- round(2*auc(test_table$def, test_table$rf_model, direction="<")-1, 4)
+# Adaboost
+gbm_gini <- round(2*auc(test_table$def, test_table$gbm_model, direction="<")-1, 4)
+# XGBoost
+xgb_gini <- round(2*auc(test_table$def, test_table$xgb_model, direction="<")-1, 4)
+
+test_ginis <- c(logit_gini, rf_gini, gbm_gini, xgb_gini)
+
+
+test_comparison <- as.data.frame(models)
+test_comparison <- cbind(test_comparison, test_ginis)
+colnames(test_comparison) <- c("model_name", "gini")
+
+View(test_comparison)
+
+
+# ------------------------------------------------------------------------------ # Cutting the score on test
+
+test_table$def <- as.numeric(ifelse(test_table$def == "yes", 0, 1))
+test_table$xgb_score<-(660-40/log(1/2)*log(1/72))+40/log(1/2)*log(test_table$xgb_model)
+score_cut <- smbinning(test_table[,c("def", "xgb_score")], y="def", x="xgb_score", p=0.1)
+score_cut$ivtable
+
+
+# ------------------------------------------------------------------------------ # Cutting the score on all data
+
+whole_table$def <- as.numeric(ifelse(whole_table$def == "yes", 0, 1))
+whole_table$xgb_score<-(660-40/log(1/2)*log(1/72))+40/log(1/2)*log(whole_table$xgb_model)
+score_cut <- smbinning(whole_table[,c("def", "xgb_score")], y="def", x="xgb_score", p=0.13)
+score_cut$ivtable
+
+# Cutpoint CntRec CntGood CntBad CntCumRec CntCumGood CntCumBad PctRec GoodRate BadRate     Odds LnOdds     WoE     IV
+# 1 <= 473.0546  19503   13406   6097     19503      13406      6097 0.1300   0.6874  0.3126   2.1988 0.7879 -1.8484 0.9470
+# 2 <= 518.3472  30355   27971   2384     49858      41377      8481 0.2024   0.9215  0.0785  11.7328 2.4624 -0.1739 0.0066
+# 3 <= 565.6915  29687   28804    883     79545      70181      9364 0.1979   0.9703  0.0297  32.6206 3.4849  0.8487 0.0999
+# 4 <= 589.5328  29007   28600    407    108552      98781      9771 0.1934   0.9860  0.0140  70.2703 4.2523  1.6161 0.2646
+# 5 <= 609.4681  21228   21050    178    129780     119831      9949 0.1415   0.9916  0.0084 118.2584 4.7729  2.1366 0.2834
+# 6  > 609.4681  20220   20143     77    150000     139974     10026 0.1348   0.9962  0.0038 261.5974 5.5668  2.9305 0.3992
+# 7     Missing      0       0      0    150000     139974     10026 0.0000      NaN     NaN      NaN    NaN     NaN    NaN
+# 8       Total 150000  139974  10026        NA         NA        NA 1.0000   0.9332  0.0668  13.9611 2.6363  0.0000 2.0007
--- models.R
+++ models.R
@@ -11,10 +11,21 @@ set.seed(361309)
 # load("data/train_woe_smote.Rdata")
 load("data/train_woe.Rdata")
 load("data/validation_woe.Rdata")
+load("data/test_woe.Rdata")
 
 result_table <- as.data.frame(validation_woe$def)
 colnames(result_table) <- "def"
 
+train_result_table <- as.data.frame(train_woe$def)
+colnames(train_result_table) <- "def"
+
+test_table <- as.data.frame(test_woe$def)
+colnames(test_table) <- "def"
+
+
+whole_woe <- rbind(train_woe, validation_woe, test_woe)
+whole_table <- as.data.frame(whole_woe$def)
+colnames(whole_table) <- "def"
 
 # ------------------------------------------------------------------------------ Logistic Regression
 
@@ -33,7 +44,8 @@ logit_model <- glm(def ~ RevolvingUtilizationOfUnsecuredLines +
                    family=binomial("logit"))
 summary(logit_model)
 
-result_table$logit_model<-predict(logit_model, newdata=validation_woe, type="response") 
+result_table$logit_model<-predict(logit_model, newdata=validation_woe, type="response")
+test_table$logit_model<-predict(logit_model, newdata=test_woe, type="response") 
 
 # ROC
 roc_logit <- roc(result_table$def, result_table$logit_model)
@@ -46,7 +58,8 @@ plot(roc_logit, print.auc=TRUE)
 # 0.6423601
 
 # Scoring
-result_table$logit_score<-(660-40/log(1/2)*log(1/72))+40/log(1/2)*predict(logit_model, newdata=validation_woe, type="link") 
+result_table$logit_score<-(660-40/log(1/2)*log(1/72))+40/log(1/2)*predict(logit_model, newdata=validation_woe, type="link")
+train_result_table$logit_score<-(660-40/log(1/2)*log(1/72))+40/log(1/2)*predict(logit_model, newdata=train_woe, type="link")
 
 hist(result_table[result_table$def=="no",c("logit_score")])
 hist(result_table[result_table$def=="yes",c("logit_score")],add=TRUE,col="red", alpha=I(0.5))
@@ -68,19 +81,21 @@ rf_model <- randomForest(x = train_woe[,-6],
 
 
 result_table$rf_model <- predict(rf_model, validation_woe, type = "prob")[,2]
+test_table$rf_model <- predict(rf_model, test_woe, type = "prob")[,2]
 
 # ROC
 roc_rf <- roc(result_table$def, result_table$rf_model)
 auc(roc_rf)
 plot(roc_rf, print.auc=TRUE)
-# AUC =  0.8135
+# AUC =  0.8142
 
 # GINI
 2*auc(result_table$def, result_table$rf_model, direction="<")-1
-# 0.6270622
+# 0.6284605
 
 # Scoring
 result_table$rf_score<-(660-40/log(1/2)*log(1/72))+40/log(1/2)*ifelse(log(result_table$rf_model) == -Inf, -5, log(result_table$rf_model))
+train_result_table$rf_score<-(660-40/log(1/2)*log(1/72))+40/log(1/2)*ifelse(log(predict(rf_model, train_woe, type = "prob")[,2]) == -Inf, -5, predict(rf_model, train_woe, type = "prob")[,2])
 
 hist(result_table[result_table$def=="no",c("rf_score")])
 hist(result_table[result_table$def=="yes",c("rf_score")],add=TRUE,col="red", alpha=I(0.5))
@@ -104,21 +119,23 @@ gbm_model <- gbm(data = train_woe[,-6],
                  verbose = TRUE)
 
 result_table$gbm_model <- predict(gbm_model, validation_woe, n.trees = 80, type = "response")
+test_table$gbm_model <- predict(gbm_model, test_woe, n.trees = 80, type = "response")
 
 # ROC
 roc_gbm <- roc(result_table$def, result_table$gbm_model)
 auc(roc_gbm)
 plot(roc_gbm, print.auc=TRUE)
-# AUC = 0.8507
+# AUC = 0.851
 
 # GINI
 2*auc(result_table$def, result_table$gbm_model, direction="<")-1
-# 0.7014977
+# 0.7019471
 
 train_woe$def_num <- NULL
 
 # Scoring
 result_table$gbm_score<-(660-40/log(1/2)*log(1/72))+40/log(1/2)*log(result_table$gbm_model)
+train_result_table$gbm_score<-(660-40/log(1/2)*log(1/72))+40/log(1/2)*log(predict(gbm_model, train_woe, n.trees = 80, type = "response"))
 
 hist(result_table[result_table$def=="no",c("gbm_score")])
 hist(result_table[result_table$def=="yes",c("gbm_score")],add=TRUE,col="red", alpha=I(0.5))
@@ -138,6 +155,12 @@ train_woe$no_dependents <- as.numeric(ifelse(train_woe$no_dependents == "yes", 1
 validation_woe$no_income <- as.numeric(ifelse(validation_woe$no_income == "yes", 1, 0))
 validation_woe$no_dependents <- as.numeric(ifelse(validation_woe$no_dependents == "yes", 1, 0))
 
+test_woe$no_income <- as.numeric(ifelse(test_woe$no_income == "yes", 1, 0))
+test_woe$no_dependents <- as.numeric(ifelse(test_woe$no_dependents == "yes", 1, 0))
+
+whole_woe$no_income <- as.numeric(ifelse(whole_woe$no_income == "yes", 1, 0))
+whole_woe$no_dependents <- as.numeric(ifelse(whole_woe$no_dependents == "yes", 1, 0))
+
 xgb_model <- xgboost(data = as.matrix(train_woe[,-c(6, 14)]), 
                      label = as.matrix(train_woe[,14]), 
                      eta = 0.3, #0.05, 0.2, 0.25
@@ -153,6 +176,8 @@ xgb_model <- xgboost(data = as.matrix(train_woe[,-c(6, 14)]),
                      objective = "binary:logistic")
 
 result_table$xgb_model <- predict(xgb_model, as.matrix(validation_woe[,-6]))
+test_table$xgb_model <- predict(xgb_model, as.matrix(test_woe[,-6]))
+whole_table$xgb_model <- predict(xgb_model, as.matrix(whole_woe[,-6]))
 
 # ROC
 roc_xgb <- roc(result_table$def, result_table$xgb_model)
@@ -168,6 +193,7 @@ train_woe$def_num <- NULL
 
 # Scoring
 result_table$xgb_score<-(660-40/log(1/2)*log(1/72))+40/log(1/2)*log(result_table$xgb_model)
+train_result_table$xgb_score<-(660-40/log(1/2)*log(1/72))+40/log(1/2)*log(predict(xgb_model, as.matrix(train_woe[,-6])))
 
 hist(result_table[result_table$def=="no",c("xgb_score")])
 hist(result_table[result_table$def=="yes",c("xgb_score")],add=TRUE,col="red", alpha=I(0.5))
@@ -180,8 +206,11 @@ legend(locator(1), levels(result_table$def), fill=colfill)
 
 # ------------------------------------------------------------------------------ Saving models
 
-save(logit_model, file = "models/logit_model.Rdata")
-save(rf_model, file = "models/rf_model.Rdata")
-save(gbm_model, file = "models/gbm_model.Rdata")
-save(xgb_model, file = "models/xgb_model.Rdata")
-save(result_table, file = "models/result_table.RData")
+# save(logit_model, file = "models/logit_model.Rdata")
+# save(rf_model, file = "models/rf_model.Rdata")
+# save(gbm_model, file = "models/gbm_model.Rdata")
+# save(xgb_model, file = "models/xgb_model.Rdata")
+# save(result_table, file = "models/result_table.RData")
+# save(train_result_table, file = "models/train_result_table.RData")
+# save(test_table, file = "models/test_table.RData")
+# save(whole_table, file = "models/whole_table.RData")
